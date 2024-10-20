6cb0fd54c2eb5ba164ad7be45e85fd2ccecf4837
Trinh <khantrinh293@gmail.com>
2024-10-20 16:22:43 +0700

    cc

---
+++ TEDU/Scripts/js/vi.js
@@ -0,0 +1,19 @@
+// vi.js
+flatpickr.l10ns.vi = {
+    weekdays: {
+        shorthand: ['CN', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7'],
+        longhand: ['Chủ nhật', 'Thứ hai', 'Thứ ba', 'Thứ tư', 'Thứ năm', 'Thứ sáu', 'Thứ bảy']
+    },
+    months: {
+        shorthand: ['T1', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7', 'T8', 'T9', 'T10', 'T11', 'T12'],
+        longhand: ['Tháng 1', 'Tháng 2', 'Tháng 3', 'Tháng 4', 'Tháng 5', 'Tháng 6', 'Tháng 7', 'Tháng 8', 'Tháng 9', 'Tháng 10', 'Tháng 11', 'Tháng 12']
+    },
+    ordinal: () => 'th',
+    firstDayOfWeek: 1,
+    rangeSeparator: ' đến ',
+    scrollTitle: 'Cuộn để tăng',
+    toggleTitle: 'Nhấp để chuyển đổi',
+    amPM: ['SA', 'CH']
+};
+
+flatpickr.localize(flatpickr.l10ns.vi);
